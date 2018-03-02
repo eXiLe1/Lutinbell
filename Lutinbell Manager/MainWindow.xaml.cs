ebf3f2572bbd6040c165b71bd50e4136f9c951cf
Chris Ashley <cashley2K@outlook.com>
2018-03-02 09:19:26 +0000

    Initial

--- Lutinbell Manager/MainWindow.xaml.cs
+++ Lutinbell Manager/MainWindow.xaml.cs
@@ -12,17 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Lutinbell_Manager.ViewModels;
+using MahApps.Metro.Controls;
 
 namespace Lutinbell_Manager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HomeViewModel();
+        }
+
+        private void Website_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new WebsiteViewModel();
         }
     }
 }
